4323d4f4186037f5bc5ee15dc30e86bf421351c8
Fernando Gonzalez Arias <efernando.g94@gmail.com>
2024-04-08 19:41:14 -0400

    a

--- eclipse-workspace/Programacion-Orientada-Objeto/src/com/cursojava04/modulo4/instrumentos/Main.java
+++ eclipse-workspace/Programacion-Orientada-Objeto/src/com/cursojava04/modulo4/instrumentos/Main.java
@@ -3,7 +3,15 @@ package com.cursojava04.modulo4.instrumentos;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Guitarra guitarra1 = new Guitarra();
+		Guitarra guitarra2 = new Guitarra();
+		Guitarra guitarra3 = new Guitarra();
+		
+		
+		guitarra1.tipo = "Electrica";
+		guitarra1.marca = "Fender";
+		guitarra1.cantidadCuerdas = 6;
+		guitarra1.color = "Black";
 
 	}
 
