af731d0ca1039dc24b3c2cfee7e7831f526402d3
Fernando Gonzalez Arias <efernando.g94@gmail.com>
2024-04-08 19:31:39 -0400

    j

---
+++ eclipse-workspace/Programacion-Orientada-Objeto/src/com/cursojava04/modulo4/instrumentos/Guitarra.java
@@ -0,0 +1,22 @@
+package com.cursojava04.modulo4.instrumentos;
+
+public class Guitarra {
+	
+	// ATRIBUTOS
+	public String tipo;
+	public String marca;
+	public int cantidadCuerdas;
+	public String color;
+	
+	//METODOS
+	public void emitirNota() {
+		System.out.println("Emitiendo una nota");
+	}
+	
+	public void afinar() {
+		System.out.println("Afinando la guitarra");
+	}
+	
+	
+
+}
