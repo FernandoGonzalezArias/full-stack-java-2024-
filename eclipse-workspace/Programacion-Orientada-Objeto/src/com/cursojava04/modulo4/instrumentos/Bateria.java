365726e975e2f1536bc44abc66c05cd6242eaae9
Fernando Gonzalez Arias <efernando.g94@gmail.com>
2024-04-08 19:35:35 -0400

    eliminargitignore

--- eclipse-workspace/Programacion-Orientada-Objeto/src/com/cursojava04/modulo4/instrumentos/Bateria.java
+++ eclipse-workspace/Programacion-Orientada-Objeto/src/com/cursojava04/modulo4/instrumentos/Bateria.java
@@ -3,5 +3,20 @@ package com.cursojava04.modulo4.instrumentos;
 public class Bateria {
 	public String macra;
 	public String tipo;
+	public int cantidadPlatos;
+	public String color;
+	public boolean doblePedal;
+	
+	public void tocarRedoble() {
+		System.out.println("tacatacataca");
+	}
+	
+	public void afinar() {
+		System.out.println("afinando bateria");
+	}
+	
+	public void montarBateria() {
+		System.out.println("Montando bateria");
+	}
 
 }
